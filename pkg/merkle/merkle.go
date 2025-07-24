0b4dee1418ff00c4d7cf58266c78b112261eb9fe
mkdior <h@mza.jp>
2025-07-24 19:53:00 +0900

    + -- add initial structs for hashers and merkle tree

---
+++ pkg/merkle/merkle.go
@@ -0,0 +1,41 @@
+package merkle
+
+// Basic structure the merkle tree package will generate once consumed.
+// R -> Root | H -> Hash | L -> Leaf
+// Note: Leafs are doubly hashed with their respective tags, so are the
+// intermediate nodes, even though those are listed as being hashed once
+// without their tags. Just for illustrative purposes.
+// ---------------------------------------------------------------------
+//							  +---------------------+
+//							  | R(H(H(A+B)+H(C+D))) |
+//							  +---------------------+
+// 											 	  /	\
+// 											 	 /	 \
+// 											 	/			\
+// 											 /			 \
+// 											/					\
+//						+--------+					 +--------+
+//						| H(A+B) |					 | H(C+D) |
+//						+--------+					 +--------+
+//                /\    			  			 /\
+//			         /	\   			        /	 \
+//       +------+    +------+ +------+    +------+
+//			 | L(A) |    | L(B) | | L(C) |    | L(D) |
+//       +------+    +------+ +------+    +------+
+
+type Hash = [32]byte
+
+// Additional options to be consumed when calculating a hash.
+type HashOpts struct {
+	// Currently we support the adding of just a single tag.
+	Tag string
+}
+
+type Tree struct {
+	// Eventual merkle tree, will consume next property "leaves".
+	tree [][]Hash
+	// Initial leaves containing base leaf data.
+	leaves []Hash
+	// Indicator to check whether or not the merkle root has been calculated.
+	finalized bool
+}
