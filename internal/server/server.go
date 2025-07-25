046bc6236416505efddab08d94928b28ab03c867
mkdior <h@mza.jp>
2025-07-25 13:22:26 +0900

    + listen for os term, throw serve in gr

--- internal/server/server.go
+++ internal/server/server.go
@@ -13,25 +13,29 @@ import (
 func Start() {
 	mux := http.NewServeMux()
 	server := http.Server{
-		Addr:    "172.186.0.2:8082",
+		Addr:    ":8082",
 		Handler: mux,
 	}
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatalf("failed starting server...\n%s", err)
-	}
+	log.Print("Starting server...")
+	go func() {
+		if err := server.ListenAndServe(); err != nil &&
+			err != http.ErrServerClosed {
+			log.Fatalf("failed starting server...\n%s", err)
+		}
+	}()
 
 	mux.HandleFunc("POST /user/create", handleUserCreate)
 	mux.HandleFunc("POST /merkle/build", handleMerkleBuild)
 	mux.HandleFunc("GET /merkle/proof/generate", handleMerkleProofGenerate)
 
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGINT)
+	sig := make(chan os.Signal, 2)
+	signal.Notify(sig, os.Interrupt, syscall.SIGINT)
 	<-sig
-	log.Print("Shutting down server... gn")
+	log.Print("Shutting down server...")
 
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	err = server.Shutdown(ctx)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := server.Shutdown(ctx)
 	if err != nil {
 		log.Fatalf("failed shutting down server...\n%s", err)
 	}
