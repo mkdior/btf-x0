1e9cab463a7689d48d727566dfc42226bc0caed0
mkdior <h@mza.jp>
2025-07-26 23:01:30 +0900

    + -- implement userindex

--- internal/index/userindex.go
+++ internal/index/userindex.go
@@ -1,8 +1,10 @@
 package index
 
 import (
+	"errors"
+
 	"github.com/mkdior/btf-x0/internal/database"
-	"github.com/mkdior/btf-x0/internal/models"
+	model "github.com/mkdior/btf-x0/internal/models/user"
 )
 
 type UserIndex struct {
@@ -17,6 +19,37 @@ func NewUserIndex(db database.Database) *UserIndex {
 	}
 }
 
-func (ui *UserIndex) Set(user models.User, hash [32]byte) error {}
-func (ui *UserIndex) GetByID(id int) (models.User, error)       {}
-func (ui *UserIndex) DeleteByID(id int) error                   {}
+func (ui *UserIndex) Set(user model.User, hash [32]byte) error {
+	val := model.Serialize(user)
+	data := database.DataFormat{Hash: hash, Value: val}
+
+	if err := ui.db.Set(data); err != nil {
+		return err
+	}
+	ui.idMap[user.ID] = hash
+	return nil
+}
+
+func (ui *UserIndex) GetByID(id int) (model.User, error) {
+	hash, ok := ui.idMap[id]
+	if !ok {
+		return model.User{}, errors.New("id not indexed")
+	}
+	data, err := ui.db.Get(hash)
+	if err != nil {
+		return model.User{}, err
+	}
+	return model.Deserialize(data.Value)
+}
+
+func (ui *UserIndex) DeleteByID(id int) error {
+	hash, ok := ui.idMap[id]
+	if !ok {
+		return nil
+	}
+	if err := ui.db.Delete(hash); err != nil {
+		return err
+	}
+	delete(ui.idMap, id)
+	return nil
+}
